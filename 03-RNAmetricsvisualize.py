3f170c2764ad1f2e70561f0da30fdfc63214eef2
NikeeShrestha <nshrest@okstate.edu>
2025-12-09 14:57:11 -0600

    now skewness values can be calculated

--- 03-RNAmetricsvisualize.py
+++ 03-RNAmetricsvisualize.py
@@ -1,4 +1,10 @@
-import os, pandas as pd, seaborn as sns, matplotlib.pyplot as plt
+import os, pandas as pd, seaborn as sns, matplotlib.pyplot as plt, numpy as np, matplotlib.cm as cm, matplotlib.colors as colors, sys
+
+
+inputpath=sys.argv[1]
+
+outputprefix=sys.argv[2]
+
 
 def RNAmetrics(path):
     files=os.listdir(path)
@@ -10,10 +16,70 @@ def RNAmetrics(path):
         df=pd.concat([df, tempdf], axis=0)
     return df
 
+def distribution_skew(g):
+    x = g["normalized_position"].values
+    y = g["All_Reads.normalized_coverage"].values
+    
+    w = y / y.sum()
+    mu = np.sum(w * x)
+    sigma = np.sqrt(np.sum(w * (x - mu)**2))
+    
+    if sigma == 0:
+        return 0
+    
+    skew = np.sum(w * (x - mu)**3) / (sigma**3)
+    return skew
+
 def RNAmetricslineplot(df, outputfile):
     sns.lineplot(x=df['normalized_position'], y=df['All_Reads.normalized_coverage'], hue=df['genotype'], legend=False)
     plt.savefig(str(outputfile))
     plt.show()
+
+def histoSkewness(df, outputfile):
+    plt.figure(figsize=(6,4))   
+    plt.hist(df["scaled_skew"], bins=30, edgecolor="black")
+    plt.xlim(-1,1)
+    plt.xlabel("Scaled Skewness")
+    plt.ylabel("Number of Genotypes")
+    plt.savefig(str(outputfile))
+    plt.show()
+
+def RNAmetricslineplot_withskewness(df, outputfile):
+    plt.figure(figsize=(6,4))   
+    cmap = plt.cm.coolwarm
+
+    norm = plt.Normalize(vmin=-1, vmax=1)
+
+    sns.lineplot(x=df['normalized_position'], y=df['All_Reads.normalized_coverage'], hue=df['scaled_skew'],
+             legend=False, palette=cmap,hue_norm=norm)
     
-df=RNAmetrics("/work/schnablelab/nikees/AlternateSplicing/RNAmetrics")
-RNAmetricslineplot(df, 'maizelineplot.png')
+    sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+    sm.set_array([])  # required for matplotlib <3.6
+    cbar = plt.colorbar(sm)
+    cbar.set_label("Scaled Skewness (−1 → +1)", fontsize=12)
+    
+    plt.savefig(str(outputfile))
+    plt.show()
+    
+df=RNAmetrics(str(inputpath))
+
+skew_df = df.groupby("genotype").apply(distribution_skew).reset_index(name="distribution_skew")
+
+skew = skew_df["distribution_skew"].values
+
+S_max = skew.min()
+
+skew_scaled =skew/S_max
+
+skew_df["scaled_skew"] = skew_scaled
+
+skew_df.to_csv(f"{outputprefix}_skewness_values.csv", index=False)
+
+df=pd.merge(df, skew_df, on='genotype')
+
+
+RNAmetricslineplot(df, f'{outputprefix}lineplot.png')
+
+histoSkewness(skew_df, f'{outputprefix}_SkewnessDistribution.png')
+
+RNAmetricslineplot_withskewness(df, f'{outputprefix}lineplotWithSkewness.png')
--- Readme.md
+++ Readme.md
@@ -6,4 +6,16 @@ You need aligned sorted bam files to run this script. My bam files are saved wit
 
 You can skip running 01-CreateRefflatfile.py and use the Refflat files required to run 02-* script using the provided files from the directory Refflatdata for sorghum and maize.
 
+
 After running the 02* script, run the 03-RNAmetricvisualize.py file to get the lineplot. 
+
+```
+python 03-RNAmetricvisualize.py <path To csv file from RNA metrics calculation > <outputfile prefix>
+```
+
+This will provide four outputs:
+
+1. Both raw and scaled skewness values per sample. `outputprefix_skewness_values.csv`
+2. Line plot showing normalized coverage across normalized position. `outputprefixlineplot.png`
+3. Line plot showing normalized coverage across normalized position colored by skewness value. `outputprefixlineplotWithSkewness.png`
+4. Distribution of skewness values in the population. `outputprefix_SkewnessDistribution.png`
